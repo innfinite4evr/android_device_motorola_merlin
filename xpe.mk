43238ab73eb59c019c974c5492d40c73b5ad1347
innfinite4evr <innfinite4evr@gmail.com>
2017-09-12 19:19:52 +0000

    merlin: Redefine product model
    Signed-off-by: innfinite4evr <innfinite4evr@gmail.com>

--- xpe.mk
+++ xpe.mk
@@ -22,6 +22,7 @@ TARGET_SCREEN_HEIGHT := 1280
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := merlin
 PRODUCT_NAME := xpe_merlin
+PRODUCT_MODEL := MotoG3-TE
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := merlin
