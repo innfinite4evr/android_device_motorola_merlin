a3ca7261ed2a19e773bdffdaf3b183a612ddd9ef
Shubham Singh <coolsks94@gmail.com>
2017-11-19 06:43:23 +0530

    Compile kernel with UBERTC 6.0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,10 @@ TARGET_OTA_ASSERT_DEVICE := merlin
 TARGET_INIT_VENDOR_LIB := libinit_merlin
 TARGET_RECOVERY_DEVICE_MODULES := libinit_merlin
 
+# Kernel Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-6.0/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
 # Kernel
 TARGET_KERNEL_CONFIG := merlin_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
